66217ca8b1c0c5601822158528301aea18f564df
Coleton Watt <electricninja102@gmail.com>
2021-09-30 08:12:28 -0600

    Initial Commit

---
+++ Unit 5: No Brainer: Time/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Unit 5: No Brainer: Time
+//
+//  Created by Coleton Watt on 9/30/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
